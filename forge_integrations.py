073fb3fd3dc32c349cd9356438b50483cfa92405
resistaiart <a.chosen.few@gmail.com>
2025-08-28 19:08:17 +1000

    Create forge_integrations.py

---
+++ forge_integrations.py
@@ -0,0 +1,7 @@
+# forge_integrations.py
+def list_integrations():
+    """
+    Return supported ecosystem integrations.
+    Later: enable actual connections.
+    """
+    return ["huggingface", "civitai", "comfyui-connect"]
