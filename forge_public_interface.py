940e58489f1a4df3522e1a5023b87f3fa8bfe7ac
resistaiart <a.chosen.few@gmail.com>
2025-08-29 22:30:07 +1000

    Update forge_public_interface.py

--- forge_public_interface.py
+++ forge_public_interface.py
@@ -1,4 +1,9 @@
-from typing import Literal
+# forge_public_interface.py
+from enum import Enum
 
-# Allowed generation goals recognised by the sealed workshop
-PackageGoal = Literal["t2i", "i2i", "t2v", "i2v"]
+class PackageGoal(str, Enum):
+    T2I = "t2i"  # Text-to-Image
+    T2V = "t2v"  # Text-to-Video
+    I2I = "i2i"  # Image-to-Image
+    I2V = "i2v"  # Image-to-Video
+
